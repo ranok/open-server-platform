3b760af79dde4f2adc4e22f322b0cabc103ddd10
ranok <torreyji@clarkson.edu>
2009-04-13 05:11:33 -0700

    Added the ebin path to the startup script to fix boot issues on other systems

--- start-osp.sh
+++ start-osp.sh
@@ -2,5 +2,5 @@
 
 echo "Starting OSP"
 epmd -daemon
-erl -smp auto -detached -boot osp_rel-0.4
+erl -smp auto -detached -boot osp_rel-0.4 -pa ./ebin
 echo "OSP started, telnet to localhost port 9876 to continue"
