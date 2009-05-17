d8f6d3313131dff5ba3b7b0ba43478cc95153333
Jacob I. Torrey <torreyji@clarkson.edu>
2009-05-17 09:17:09 -0400

    Converted the droppables into sortables, now to get linking working and we're all good

--- www/app.js
+++ www/app.js
@@ -3,11 +3,6 @@ $(document).ready(
 		  });
 
 $(function() { 
-	$(".draggable").draggable({helper: 'clone'});
-	$("#nodes").droppable({drop: function(event, ui) {
-		    ui.draggable.hide("puff", {}, 500, function() {});
-		}});
-	$(".droppable").droppable({greedy: true, drop: function(event, ui) {
-		    ui.draggable.clone(true).draggable().insertAfter(this).after("<br />");
-		}});
+	$(".draggable").draggable({helper: 'clone', connectToSortable: ".sortable"});
+	$(".droppable").sortable({remove: function(event, ui) {ui.item.hide()}});
     });
