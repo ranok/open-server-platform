d3542b1fb7cb338e63761ae3ca944e950ed5f062
Jacob Torrey <torreyji@clarkson.edu>
2009-10-29 18:57:24 -0400

    Added support for .sap servlet packages in compile_servlet

--- compile_servlet.pl
+++ compile_servlet.pl
@@ -10,31 +10,91 @@ if(scalar(@ARGV) eq 0) {
     exit(-1);
 }
 
-my $inputfile = $ARGV[0];
-my $parser = new XML::Simple;
+our $tempdir;
+our $inputfile = $ARGV[0];
+our $basename = $inputfile;
+$basename =~ s/\.[a-z]*$//;
+$basename =~ s/^.*\///;
+my $extension = $inputfile;
+$extension =~ s/^(.*\.)([a-z]+)$/$2/;
+my $ret;
+if($extension =~ /erl/) {
+    $ret = erl();
+} elsif($extension =~ /sdf/) {
+    $ret = sdf();
+} elsif($extension =~ /sap/) {
+    $ret = sap();
+} else {
+    print "Error, invalid filetype\n";
+    exit(-1);
+}
+if($ret eq 0) {
+    print "$basename - Compilation complete\n";
+    exit(0);
+} else {
+    print "Error in compilation of $basename\n";
+    exit($ret);
+}
 
-my $data = $parser->XMLin($inputfile);
+sub sap {
+    `mkdir $basename`;
+    `unzip -d $basename $inputfile`;
+    opendir(DIR, $basename);
+    while(defined(my $file = readdir(DIR))) {
+	if($file =~ /^\.$/ || $file =~ /^\.\.$/) {
+	    # Do nothing
+	} else {
+	    my $ret = `./compile_servlet.pl $basename/$file`;
+	    if($ret =~ /complete/) {
+		# Do nothing
+		my $generatedfile = $file;
+		$generatedfile =~ s/\.[a-z]*$//;
+		$generatedfile .= ".erl";
+		`mv -f $generatedfile $basename/`;
+	    } else {
+		return -1;
+	    }
+	}
+    }
+    closedir(DIR);
+    return 0;
+}
 
-my $author = "'".$data->{author}."'";
-my $module = $data->{name};
-my $version = "'".$data->{version}."'";
-my $code = $data->{code}->{server};
-my $initfunc = $data->{code}->{init};
-my $cleanup = $data->{code}->{cleanup};
-my $proto = $data->{proto};
+sub erl {
+    return 0;
+}
 
-
-`cat header.erl | sed -e s/AUTHOR/$author/ -e s/PROTO/$proto/ -e s/VSN/$version/ -e s/MOD/$module/g > $module.erl`;
-open(FP, ">>$module.erl");
-print FP $code;
-close(FP);
-open(FP, ">>$module.erl");
-print FP $initfunc;
-close(FP);
-open(FP, ">>$module.erl");
-print FP $cleanup;
-close(FP);
+sub sdf {
+    my $parser = new XML::Simple;
+    
+    my $data = $parser->XMLin($inputfile);
+    
+    my $author = "'".$data->{author}."'";
+    my $module = $data->{name};
+    my $version = "'".$data->{version}."'";
+    my $code = $data->{code}->{server};
+    my $initfunc = $data->{code}->{init};
+    my $cleanup = $data->{code}->{cleanup};
+    my $proto = $data->{proto};
+    
+    
+    `cat header.erl | sed -e s/AUTHOR/$author/ -e s/PROTO/$proto/ -e s/VSN/$version/ -e s/MOD/$module/g > $module.erl`;
+    open(FP, ">>$module.erl");
+    print FP $code;
+    close(FP);
+    open(FP, ">>$module.erl");
+    print FP $initfunc;
+    close(FP);
+    open(FP, ">>$module.erl");
+    print FP $cleanup;
+    close(FP);
 #`erlc $module.erl`;
 #`rm $module.erl`;
 
-print "$module generated successfully!\nRun erlc $module.erl to compile\n";
+    if (-e "$module.erl") {
+	return 0;
+    } else {
+	return -1;
+    }
+
+}
