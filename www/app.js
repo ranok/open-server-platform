80a24d67b5e4e48f21ccea6b35322181adc563dc
Jacob Torrey <torreyji@clarkson.edu>
2009-10-31 23:35:35 -0400

    Revert "Added delete link which only appears for apps in running nodes."
    This reverts commit 93f929dcebcc2720640e358584ca9bf4160684e6.

--- www/app.js
+++ www/app.js
@@ -5,19 +5,12 @@ $(function() {
     });
 
     $(".droppable").sortable({
-        connectWith: '.droppable',
-        stop: function(event, ui) {
-		if ($(ui.item).children("a").size() == 0) {
-		    $(ui.item).append('<br /><br /><a href="#" class="delete_app">Delete</a>');
+	/*
+	remove: function(event, ui) {
+	            ui.item.hide();
 		}
-	}
-    });
+	*/
 
-    $("body").click(function(event) {
-	var $target = $(event.target);
-        if ($target.is(".delete_app")) {
-	    event.preventDefault();
-	    $target.parent().remove();
-	}
+        connectWith: '.droppable'
     });
 });
