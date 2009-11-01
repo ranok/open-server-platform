93f929dcebcc2720640e358584ca9bf4160684e6
Ryan Lewis <lewisrj@laptap.(none)>
2009-10-31 20:23:25 -0700

    Added delete link which only appears for apps in running nodes.

--- www/app.js
+++ www/app.js
@@ -5,12 +5,19 @@ $(function() {
     });
 
     $(".droppable").sortable({
-	/*
-	remove: function(event, ui) {
-	            ui.item.hide();
+        connectWith: '.droppable',
+        stop: function(event, ui) {
+		if ($(ui.item).children("a").size() == 0) {
+		    $(ui.item).append('<br /><br /><a href="#" class="delete_app">Delete</a>');
 		}
-	*/
+	}
+    });
 
-        connectWith: '.droppable'
+    $("body").click(function(event) {
+	var $target = $(event.target);
+        if ($target.is(".delete_app")) {
+	    event.preventDefault();
+	    $target.parent().remove();
+	}
     });
 });
