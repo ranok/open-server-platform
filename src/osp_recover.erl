8dde415b5a29575b061e7ab7f96d3df42d131250
Jacob Torrey <torreyji@clarkson.edu>
2009-11-01 11:49:22 -0500

    Added the start of a smart auto-revoery system

---
+++ src/osp_recover.erl
@@ -0,0 +1,7 @@
+%% @author Jacob Torrey <torreyji@clarkson.edu>
+%% @copyright 2009 Jacob Torrey
+%% @doc Provides the functionality for a node to recover any running applications after a crash
+-module(osp_recover).
+
+-export([]).
+
