5f4298cad4685a19c739872a138a546869644f3b
Jacob Torrey <torreyji@clarkson.edu>
2009-11-09 14:17:46 -0500

    Will now fail if OSP doesn't start

--- start-osp.sh
+++ start-osp.sh
@@ -6,5 +6,10 @@ echo "http://www.openserverplatform.com"
 epmd -daemon
 erl -smp auto -detached -mnesia dir "mnesia" -boot osp_rel-0.4 -pa ./ebin
 sleep 10
+nc localhost -w 0 9876 > /dev/null
+if [ $? -eq 1 ]; then
+    echo "There was an error starting OSP"
+    exit -1
+fi
 echo "Open Server Platform started"
 echo "Telnet to localhost port 9876 or go to http://localhost:9877 to continue"
