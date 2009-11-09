cc6b9393f6cda27144c7f63b0db1838767505ef8
Jacob Torrey <torreyji@clarkson.edu>
2009-11-09 14:56:42 -0500

    Refactored some of the backend, and continued work on the compile aspect

--- src/osp_web.erl
+++ src/osp_web.erl
@@ -2,7 +2,7 @@
 %% @copyright 2009 Jacob Torrey <torreyji@clarkson.edu>
 %% @doc Provides the httpd and web interface for OSP
 -module(osp_web).
--export([start/0, reload/0, stop/1, clusterwide/3]).
+-export([start/0, reload/0, stop/1, clusterwide/3, upload/3]).
 
 -define(CONF_FILE, "include/httpd.conf").
 
@@ -39,6 +39,16 @@ nl2br(Str) ->
     Tokens = string:tokens(Str, "$\n"),
     string:join(Tokens, "\n<br />").
 
+%% @doc Adds support for uploading a servlet
+%% @spec upload(any(), list(), string()) -> ok, {error, Reason}
+upload(Session, _Env, Input) ->
+    mod_esi:deliver(Session, io_lib:format("~p", [Input])).
+
+%% @doc Writes a servlet to disk and returns its filename
+%% @spec parse_upload(string()) -> string()
+parse_upload(Input) ->
+    
+
 %% @doc Provides cluster wide ajax callbacks for the web administrative system
 %% @spec(any(), list(), string()) -> ok | {error, Reason}
 clusterwide(Session, _Env, Input) ->
@@ -46,19 +56,35 @@ clusterwide(Session, _Env, Input) ->
     {value, {operation, Op}} = lists:keysearch(operation, 1, Args),
     case Op of
 	"shutdown" ->
-	    mod_esi:deliver(Session, "Content-type: text/plain\r\n\r\nOSP Shutdown"),
+	    mod_esi:deliver(Session, "Content-type: text/plain\r\n\r\n" ++ "OSP Shutdown"),
 	    osp_manager:shutdown_osp();
 	"stats" ->
-	    mod_esi:deliver(Session, "Content-type: text/html\r\n\r\n" ++ nl2br(osp_manager:stats()));
+	    mod_esi:deliver(Session, ct_string(html) ++ nl2br(osp_manager:stats()));
 	"uptime" ->
-	    mod_esi:deliver(Session, "Content-type: text/plain\r\n\r\n" ++ osp_manager:uptime());
+	    mod_esi:deliver(Session, ct_string(text) ++ osp_manager:uptime());
 	"nodes" ->
-	    mod_esi:deliver(Session, "Content-type: text/plain\r\n\r\n" ++ erlang:integer_to_list(length([node() | nodes()])));
+	    mod_esi:deliver(Session, ct_string(text) ++ erlang:integer_to_list(length([node() | nodes()])));
 	"appnode" ->
-	    mod_esi:deliver(Session, "Content-type: application/json\r\n\r\n" ++ json_nodeapp(osp_manager:nodeapp()));
+	    mod_esi:deliver(Session, ct_string(json) ++ json_nodeapp(osp_manager:nodeapp()));
+	"apps" ->
+	    mod_esi:deliver(Session, ct_string(json) ++ json_apps());
 	_ ->
 	    mod_esi:deliver(Session, "")
     end.
+
+%% @doc Returns a Content-Type string for a given MIME type
+%% @spec ct_string(atom()) -> string()
+ct_string(text) ->
+    "Content_type: text/plain\r\n\r\n";
+ct_string(html) ->
+    "Content_type: text/html\r\n\r\n";
+ct_string(json) ->
+    "Content_type: application/json\r\n\r\n";
+ct_string(_) ->
+    "Content_type: text/plain\r\n\r\n"
+
+json_apps() ->
+    "".
 
 %% @doc JSONizes the nodeapp information for the web application frontend
 %% @spec json_nodeapp(list()) -> string()
