92feb42c146d550a472059f4e3881c74c8e1bd8e
ranok <torreyji@clarkson.edu>
2010-03-22 16:14:21 -0400

    Fixed the wait-time warning

--- start-osp.sh
+++ start-osp.sh
@@ -5,8 +5,8 @@ echo "v0.4 (C) 2009 Jacob Torrey"
 echo "http://www.openserverplatform.com"
 epmd -daemon
 erl -smp auto -detached -mnesia dir "mnesia" -boot osp_rel-0.4 -pa ./ebin
-sleep 10
-nc localhost -w 0 9876 > /dev/null
+sleep 9
+nc localhost -w 1 9876 > /dev/null
 if [ $? -eq 1 ]; then
     echo "There was an error starting OSP"
     exit -1
