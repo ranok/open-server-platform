d9edd746005173fe67f0489a2c680b400ad9c726
cosmotron <lewis.ryan.j@gmail.com>
2010-03-23 22:01:52 -0400

    Added basic Start/Stop Application functionality.  Removed unused images.

--- www/app.js
+++ www/app.js
@@ -4,10 +4,10 @@ $(function() {
             var app_list = "<ul>";
             $.each(apps,
                    function(index, app_name) {
-                       app_list += '<li class="draggable border" id="' + app_name + '">' + app_name + ' Port: <input type="text" style="width: 3em;" id="' + app_name + '-port" /></li>';
+                       app_list += '<li class="draggable border" id="applist-' + app_name + '">' + app_name  + '</li>';
                    });
             app_list += "</ul>";
-            $("#app_list_loading").remove();
+            $("#app-list-loading").remove();
             $("#apps").append(app_list);
 
             $(".draggable").draggable({
@@ -21,40 +21,89 @@ $(function() {
 		$.each(nodes,
 		       function(node_num, n) {
 			 var node_block = "";
-			 node_block = '<div id="node_' + node_num + '" class="border node" style="float: left; margin-right: 20px;">' +
+			 node_block = '<div id="node-' + node_num + '" class="border node" style="float: left; margin-right: 20px;">' +
 				      '  <h4 class="center">' + n.node + '</h4>' +
 				      '  <ul class="sortable droppable">';
 			 $.each(n.running_apps,
 				function(app_num, app) {
-				  node_block += '<li class="draggable border" id="app_' + app_num +'">' +
-				                  app.name + ' Port: <input type="text" style="width: 3em;" id="app_' + app_num + '_port" value="' + app.port + '"/>' +
-				                '<br /><br /><a href="#" id="stop_app_' + app_num + '" class="stop_app">Stop</a>' +
+				  node_block += '<li class="draggable border" id="app-' + app.name +'">' + app.name +
+                                                '<br />Port: ' + app.port +
+				                '<br /><br /><a href="#" id="stop-app-' + app.name + '" class="stop-app">Stop</a>' +
 				                '</li>';
 				});
 			 node_block += '    <li></li>' +
 				       '  </ul>' +
 				       '</div>';
 
-			 $("#node_loading").remove();
+			 $("#node-loading").remove();
 			 $("#nodes").append(node_block);
-		       });
+		});
+
 		$("#nodes").append('<br style="clear: both;" />');
 
 		$(".droppable").sortable({
 		    connectWith: '.droppable',
 		    stop: function(event, ui) {
 		            if ($(ui.item).children("a").size() == 0) {
-		              $(ui.item).append('<br /><br /><a href="#" class="stop_app">Stop</a>');
+                                var app_name = $(ui.item).attr("id").substring(8);
+
+                                $("#dialog").html('<form id="app-port-form" action="#"><input type="hidden" name="app-port-name" value="' + app_name +
+                                                  '" /><p>Port: <input type="text" style="width: 3em;" name="app-port" id="app-port" /></p>' +
+                                                  '<p><input type="submit" value="Create" /></p></form>').show().dialog();
+
+                                $("#app-port-form").submit(function(event) {
+                                    event.preventDefault();
+                                    var port = $("#app-port").val();
+                                    var node_name = $(ui.item).parents("ul").siblings("h4").text();
+                                    var start_url = "app/osp_web/clusterwide?operation=start_app&app=" + app_name + "&port=" + port + "&node=" + node_name;
+
+                                    $.ajax({
+                                            url: start_url,
+                                            success: function(msg) {
+                                                if (msg == "Application started successfully") {
+                                                    $("#dialog").dialog("destroy");
+                                                    $(ui.item).attr("id", "app-" + app_name)
+                                                    $(ui.item).append('<br />Port: ' + port + '<br /><br /><a href="#" class="stop-app">Stop</a>');
+                                                }
+                                                else if (msg == "There was an error starting the application") {
+                                                    alert("Error starting the application.");
+                                                    $("#dialog").dialog("destroy");
+                                                    $(ui.item).remove();
+                                                }
+                                                    
+                                            },
+                                            error: function(msg) {
+                                                alert("Error with the Start Application request.  Try again.");
+                                                $("#dialog").dialog("destroy");
+                                                $(ui.item).remove();
+                                            }
+                                    });
+                                });
 		            }
 		          }
 		});
-	      });
+    });
 
     $("body").click(function(event) {
 	var $target = $(event.target);
-        if ($target.is(".stop_app")) {
-	    event.preventDefault();
-	    $target.parent().remove();
+        if ($target.is(".stop-app")) {
+            event.preventDefault();
+            var app_name = $target.parent().attr("id").substring(4);
+	    var node_name = $target.parents("ul").siblings("h4").text();
+	    var stop_url = "app/osp_web/clusterwide?operation=stop_app&app=" + app_name + "&node=" + node_name;
+
+            $.ajax({
+		    url: stop_url,
+                    success: function(msg) {
+                        if (msg == "Application stopped successfully")
+                          $target.parent().remove();
+                        else if (msg == "There was an error stopping the application")
+                          alert("Error stopping the application.");
+                    },
+                    error: function(msg) {
+                        alert("Error with the Stop Application request.  Try again.");
+                    }
+            });
 	}
     });
 });
